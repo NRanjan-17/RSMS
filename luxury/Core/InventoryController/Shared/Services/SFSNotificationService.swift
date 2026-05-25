8cffd401fca9572fe3cf2498ebbf37aea81ae44f
NRanjan-17 <nalinishranjan05@gmail.com>
2026-05-25 09:53:49 +0530

    Update SFSNotificationService.swift

--- luxury/Core/InventoryController/Shared/Services/SFSNotificationService.swift
+++ luxury/Core/InventoryController/Shared/Services/SFSNotificationService.swift
@@ -49,7 +49,7 @@ final class SFSNotificationService {
         }
         
         Task {
-            await channel?.subscribe()
+            try? await channel?.subscribeWithError()
         }
     }
     
