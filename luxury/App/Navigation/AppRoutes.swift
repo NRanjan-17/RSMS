53d74efe1c1d265e1ec33f140eed6b5f219e6264
NRanjan-17 <nalinishranjan05@gmail.com>
2026-06-01 16:02:03 +0530

    Fix duplicate editProfile in ICRoute

--- luxury/App/Navigation/AppRoutes.swift
+++ luxury/App/Navigation/AppRoutes.swift
@@ -64,7 +64,7 @@ enum ICRoute: Hashable {
     case varianceReport(RSMSCycleCount)
     case serialCertificate
     case sfsOrders
-    case editProfile
+
     case sfsVerification(PurchasedItemEntity)
     case endlessAisleSelection
     case alerts
